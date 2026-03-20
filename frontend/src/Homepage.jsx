d816b6da3947c5b834efe1642b8bf3ce917398af
Ananta <theayushiananta@gmail.com>
2026-03-20 12:22:15 +0530

    changes

--- frontend/src/Homepage.jsx
+++ frontend/src/Homepage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import API from "../api/axios";
+import API from "../api/axios.js";
 
 import GamePage from './GamePage';
 import Loader from './Loader';
