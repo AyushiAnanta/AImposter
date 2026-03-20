64c2d72782f4af8c5dc7912b4ca01f3c8cab87ac
Ananta <theayushiananta@gmail.com>
2026-03-20 12:41:08 +0530

    CORS resolve

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,15 +5,27 @@ import cookieParser from "cookie-parser";
 const app = express();
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
-}))
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+
+    if (
+      origin.includes("vercel.app") ||
+      origin.includes("localhost")
+    ) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
 
 //configuratios!!!!!!!!!!!!!!!!!!1
 app.use(express.json({limit: "4kb"}))
 app.use(express.urlencoded({extended: true, limit: "4kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+app.options("*", cors());
 
 import gameRoutes from "./routes/gameRoutes.js";
 
